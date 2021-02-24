064c5ca70ef5fe0c705fd70c3b964a3f3b23fdb2
leoportogtr86 <leoporto_studio@hotmail.com>
2021-02-24 09:53:10 -0300

    detalhes no header

--- src/app/components/template/header/header.component.ts
+++ src/app/components/template/header/header.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+
+  title: string = "Meus Estudos de Angular!"
+
+  
+
   constructor() { }
 
   ngOnInit(): void {
+
+
   }
 
 }
